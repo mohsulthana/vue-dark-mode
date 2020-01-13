589f04fc0aadafa742c5773ea868a63111f67623
Mohammad Sulthan <muhammadsultana@outlook.com>
2020-01-13 23:26:28 +0700

    move component from header to root

--- src/App.vue
+++ src/App.vue
@@ -17,23 +17,22 @@
         <Card />
         <Card />
         <Card />
-      <div class="paddles">
-        <button @click="scroll_left" class="left-paddle paddle hidden">
-          PREV
-        </button>
         <button @click="scroll_right" class="right-paddle paddle">
-          >
+          <img src="https://previews.123rf.com/images/eljanstock/eljanstock1811/eljanstock181109222/111703684-right-arrow-vector-icon-isolated-on-transparent-background-right-arrow-transparency-logo-concept.jpg" height="40" width="40" alt="">
         </button>
-      </div>
+        <button class="scoreboard-btn">FULL SCOREBOARD</button>
     </div>
-    <Header />
+    <Toggle :mode="mode" @toggle="toggle"/>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur, est suscipit. Enim aliquid ratione quidem deserunt officia alias dolore voluptatem itaque consequatur quia. Quos non alias quidem illo minima sequi.</p>
+    <p style="display: block"><strong>Press 't' to switch the toggle</strong></p>
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header";
 import Card from "@/components/Card";
 import Select from "@/components/Select";
+import Toggle from '@/components/Toggle';
+
 
 export default {
   name: 'app',
@@ -45,9 +44,17 @@ export default {
   components: {
     Card,
     Select,
-    Header
+    Toggle
+  },
+  created () {
+    window.addEventListener('keyup', this.keyPress)
   },
   methods: {
+    keyPress (e) {
+      if (e.key === 't') {
+        this.toggle()
+      }
+    },
     scroll_left () {
       let content = document.querySelector(".scrolling-wrapper");
       content.scrollLeft -= 50;
@@ -68,33 +75,6 @@ export default {
 </script>
 
 <style>
-.scrolling-wrapper {
-  display: flex;
-  overflow-x: auto;
-  overflow-y: hidden;
-  height: 130px;
-  position: relative;
-  -webkit-overflow-scrolling: touch;
-}
-.scrolling-wrapper .card {
-  flex: 0 0 auto;
-}
-.hidden {
-	display: none;
-}
-.paddle {
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	width: 3em;
-}
-.left-paddle {
-	left: 0;
-}
-.right-paddle {
-	right: 0;
-  position: fixed;
-}
 * {
   margin: 0;
   padding: 0;
@@ -108,9 +88,41 @@ export default {
   color: #152020;
   transition: background 0.3s ease-in-out;
 }
-
 .dark {
   background: #192734;
   color: #f3f3f3;
+}
+.scrolling-wrapper {
+  display: flex;
+  overflow-x: auto;
+  overflow-y: hidden;
+  height: 130px;
+  position: relative;
+  -webkit-overflow-scrolling: touch;
+}
+.scoreboard-btn {  
+  position: sticky;
+  right: 20;
+}
+.scrolling-wrapper .card {
+  flex: 0 0 auto;
+}
+.hidden {
+	display: none;
+}
+.paddle {
+	position: relative;
+	top: 0;
+	bottom: 0;
+	width: 3em;
+}
+.left-paddle {
+	left: 0;
+}
+.right-paddle {
+	right: 0;
+  padding: auto 20px;
+  width: 300px;
+  position: sticky;
 }
 </style>
